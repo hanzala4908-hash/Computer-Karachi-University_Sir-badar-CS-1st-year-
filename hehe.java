20ce958d8aca248be6a81d277af7eba811d63ec9
hanzala4908-hash <hanzala4908@gmail.com>
2026-04-10 16:12:19 +0500

    Add files via upload

---
+++ hehe.java
@@ -0,0 +1,9 @@
+// A simple Java program to print "Hello World"
+public class hehe {
+
+    public static void main(String[] args) {
+        // Print text to the console
+        System.out.println("Hello World");
+    }
+}
+
